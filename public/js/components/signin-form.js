b0ffb0a6f59ca4696b3445254a2fcef042435fca
Antonella Enriquez Ledesma <your-email-address~>
2024-05-23 21:46:37 +0200

    fixed index

---
+++ public/js/components/signin-form.js
@@ -0,0 +1,18 @@
+const admin=[
+    {
+        "username": "Elena",
+        "password": "12345" 
+    },
+    {
+        "username": "Pablo",
+        "password": "12345"
+    }
+];
+
+export function modalClearLoginForm() {
+    const usernameImput = document.getElementById('username');
+    const passwordImput= document.getElementById('password');
+    const loginErrorSpan= document.getElementById('form-error')
+}
+
+
