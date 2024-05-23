e16429083e997128caed53db0d11ef63a3dd10e1
Antonella Enriquez Ledesma <your-email-address~>
2024-05-23 22:02:29 +0200

    fixed index.html

--- public/js/components/signin-form.js
+++ public/js/components/signin-form.js
@@ -12,7 +12,37 @@ const admin=[
 export function modalClearLoginForm() {
     const usernameImput = document.getElementById('username');
     const passwordImput= document.getElementById('password');
-    const loginErrorSpan= document.getElementById('form-error')
-}
+    const loginErrorSpan= document.getElementById('form-error');
+
+    usernameImput.value= '';
+    passwordImput.value='';
+    loginErrorSpan.textContent='';
+};
+ export function runLoginForm() {
+    const loginFormElement= document.getElementById('login-form');
+
+    loginFormElement.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const usernameImput= document.getElementById('username');
+        const passwordImput= document.getElementById('password');
+        
+
+        const usernameImputValue= usernameImput.value;
+        const passwordImputValue= passwordImput.value;
+
+        if(!userValidation(usernameImputValue, passwordImputValue)){
+            usernameImputValue='';
+            passwordImputValue='';
+            return;
+        }
+
+        clearLoginForm
+
+
+        
+    })
+    
+ }
 
 
--- public/js/components/signin-modal.js
+++ public/js/components/signin-modal.js
@@ -2,7 +2,7 @@ export function showLoginModal() {
     const OpenModalButton = document.getElementById('sig-in-button');
     const loginModal = document.getElementById('login-modal');
 
-    OpenModalButton.addEventListener('click',function() {
+    OpenModalButton.addEventListener('click', function() {
         if (!loginModal.classList.contains('modal--open')) {
             loginModal.classList.add('modal--open')
             
@@ -16,7 +16,7 @@ export function closeLoginModal(){
     const closeModalButton = document.getElementById('close-modal');
     const loginModal = document.getElementById('login-modal');
 
-    closeLoginModal.addEventListener('click',() => {
+    closeModalButton.addEventListener('click', () => {
         if (loginModal.classList.contains('modal--open')) {
             loginModal.classList.remove('modal--open')
             
