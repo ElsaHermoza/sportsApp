47d9d1117238c99f439a4da3841755aab2ae0818
Olena-Andrushchenko <olena.anru.anru@gmail.com>
2024-05-29 04:25:37 +0200

    created checkLocalStorage function, created a demo object to test

--- public/js/components/team-list-manager.js
+++ public/js/components/team-list-manager.js
@@ -7,6 +7,7 @@ export function teamListManager() {
 
     const teamAddForm = document.getElementById('team-add-form')
 
+    // Get the form and input value
     teamAddForm.addEventListener('submit', (event) => {
         event.preventDefault();
         console.log('clicked submit');
@@ -14,7 +15,41 @@ export function teamListManager() {
         const teamName = document.getElementById('team-name').value
         console.log(teamName);
 
+        return teamName
     })
 
-    
+
+    const teamListKey = 'teams'
+    let teamArray = [
+        {
+        'id': 'id',
+        'name': 'team name',
+        'flag': '/public/assets/flags/flag-placeholder.jpg'
+        }
+    ]
+
+    // Check if data exists on localStorage or Set it
+    function checkLocalStorage() {
+        
+        let teamData = localStorage.getItem(teamListKey)
+
+        // if doesnt exist, we stringify array, and assign array to teamData
+        if(!teamData) {
+            localStorage.setItem(teamListKey, JSON.stringify(teamArray))
+            teamData = teamArray;
+        } else {
+            // if exist we parse the Original teamData and assign to team data
+            teamData = JSON.parse(teamData) 
+        }
+        
+        // return the teamData from 1 of the conditions
+        console.log(teamData)
+        return teamData
+    }
+
+
+
+
+    // Function calls
+    checkLocalStorage()
 }
