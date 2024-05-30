dfa0277bafb7579c9f93e80087b63101c3134345
ElsaH <evihermoza@hotmail.com>
2024-05-30 12:48:04 +0200

    deleted id from div flags-list and created flagsArrayModal on js

--- public/js/components/flags-modal.js
+++ public/js/components/flags-modal.js
@@ -26,3 +26,14 @@ export function closeFlagsModal() {
         
      });
  };
+
+ const flagsArrayModal = [ '/public/assets/flags/flag01.jpg', '/public/assets/flags/flag02.jpg', '/public/assets/flags/flag03.jpg', '/public/assets/flags/flag04.jpg', 
+ '/public/assets/flags/flag05.jpg', '/public/assets/flags/flag06.jpg', '/public/assets/flags/flag07.jpg', '/public/assets/flags/flag08.jpg', '/public/assets/flags/flag09.jpg',
+ '/public/assets/flags/flag10.jpg', '/public/assets/flags/flag11.jpg', '/public/assets/flags/flag12.jpg', '/public/assets/flags/flag13.jpg', '/public/assets/flags/flag14.jpg', 
+ '/public/assets/flags/flag15.jpg', '/public/assets/flags/flag16.jpg'
+]
+
+export function selectFlag(){ 
+       const flagsList = document.getElementById
+
+    }
